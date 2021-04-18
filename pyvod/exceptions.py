bb80559c07bf485f0c1418aabc1a93137ae88552
sixP-NaraKa <71403526+sixP-NaraKa@users.noreply.github.com>
2021-04-18 02:56:22 +0200

    [ADD] 2 custom exceptions
    - related to the utils 'validate_path' function

--- pyvod/exceptions.py
+++ pyvod/exceptions.py
@@ -4,3 +4,12 @@ class TwitchApiException(Exception):
     """ An exception for when the Twitch API response fails / does not respond with status code 200. """
     pass
 
+
+class DirectoryDoesNotExistError(Exception):
+    """ An exception for when a supplied directory path does not exist. """
+    pass
+
+
+class DirectoryIsAFileError(Exception):
+    """ An exception for when a supplied directory path points to a file instead of a folder. """
+    pass
