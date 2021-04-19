59ba5e508493f598794e55679a1ee3c6a167b2d3
sixP-NaraKa <71403526+sixP-NaraKa@users.noreply.github.com>
2021-04-20 01:34:35 +0200

    Update vod.py

--- pyvod/vod.py
+++ pyvod/vod.py
@@ -2,8 +2,15 @@ from .vodchat import VODChat
 
 
 class VOD:
+    """ Represents a Twitch.tv VOD (video-on-demand).
+
+        The main entry point, is solely responsible for getting the VODChat via `get_videochat()`.
+
+        :param vod_id: the VOD ID to fetch the information for
+    """
+
     def __init__(self, vod_id):
-        self.vod_id = vod_id
+        self.vod_id = str(vod_id)
 
     def get_vodchat(self) -> VODChat:
         vod_chat = VODChat(vod_id=self.vod_id)
